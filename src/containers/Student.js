5db64a8d522a0fa66836e2e2e8f13b42eb2f6e39
Denis Lafontant <icebox827@gmail.com>
2021-08-22 17:36:33 -0400

    Create filter function

--- src/containers/Student.js
+++ src/containers/Student.js
@@ -1,19 +1,15 @@
-import { GridItem, Text, Grid, Box, Divider } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { GridItem, Text, Grid, Box, Divider, Input } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Image } from 'react-bootstrap';
 import { fetchStudent } from '../actions/index';
 import { Loader } from '../components/Loading';
 import { Error } from '../components/Error';
-import { Search } from '../components/Search';
 
 const Students = () => {
   const dispatch = useDispatch();
+  const [searchName, setSearchName] = useState('');
   const { students, loading, error } = useSelector((state) => state.students);
-
-  const handleChange = (students) => {
-    dispatch(fetchStudent(students));
-  };
 
   useEffect(() => {
     dispatch(fetchStudent());
@@ -36,9 +32,20 @@ const Students = () => {
 
   return (
     <Container className="card">
-      {/* <Students filterChange={handleChange} /> */}
+      <Input 
+        type="search" 
+        placeholder="Search by name..."  
+        className="search" 
+        onChange={e => {setSearchName(e.target.value)}} 
+      />
       {
-        students.map((student) => (
+        students.filter((val) => {
+          if (searchName === '') {
+            return val;
+          } else if (val.firstName.toLowerCase().includes(searchName.toLowerCase()) || val.lastName.toLowerCase().includes(searchName.toLowerCase())) {
+            return val;
+          }
+        }).map((student) => (
           
             <Grid templateColumns="repeat(3, 1fr)" key={student.id}>
               <GridItem>
